748830f27c3bfacdf8a88d6458f93c90749ef940
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 12:04:27 -0700

    add SEO

--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import SEO from '../components/SEO';
 
 export default function SlicemasterPage({ data: { person } }) {
-  console.log(person);
   return (
-    <div className="center">
-      <Img fluid={person.image.asset.fluid} />
-      <h2>
-        <span className="mark">{person.name}</span>
-      </h2>
-      <p>{person.description}</p>
-    </div>
+    <>
+      <SEO title={person.name} image={person.image?.asset?.fluid?.src} />
+      <div className="center">
+        <Img fluid={person.image.asset.fluid} />
+        <h2>
+          <span className="mark">{person.name}</span>
+        </h2>
+        <p>{person.description}</p>
+      </div>
+    </>
   );
 }
 
 export const query = graphql`
-  query($slug: String!) {
+  query ($slug: String!) {
     person: sanityPerson(slug: { current: { eq: $slug } }) {
       name
       id
