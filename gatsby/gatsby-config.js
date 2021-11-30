b7edf4501abbe1bc1ca1152fb3d205dcdcf8a3db
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 12:01:03 -0700

    add SEO

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -7,8 +7,10 @@ export default {
     title: `Gatsbys Pizza`,
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Salt Lake City!',
+    twitter: '@slicksSlices',
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
       // this is the name of the plugin you are adding
--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function OrderPage() {
   return (
     <>
+      <SEO title="Order a Pizza!" />
       <p>Hey! I'm the Order page</p>
     </>
   );
