4d5203f96529b330062ab69be1f612116a76eb13
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 11:58:56 -0700

    add SEO

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
+import SEO from '../components/SEO';
 
 const BeerGridStyles = styled.div`
   display: grid;
@@ -26,6 +27,7 @@ const SingleBeerStyles = styled.div`
 export default function BeersPage({ data }) {
   return (
     <>
+      <SEO title={`Beers! We have ${data.beers.nodes.length} in stock`} />
       <h2 className="center">
         We have {data.beers.nodes.length} Beers Available. Dine in Only!
       </h2>
