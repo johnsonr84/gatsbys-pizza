898eeaf5ec2e24b5f1897c21355dfec7b112f5f9
Your Name <robertcjohnson1984@gmail.com>
2021-11-29 22:37:58 -0700

    npm installs

--- gatsby/gatsby-config.js
+++
@@ -1,1 +0,0 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
