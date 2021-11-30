e595bf38beffbb6052fe707f11d60fa7a00e7fb9
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 13:13:55 -0700

    update with storeSettings

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -7,11 +7,12 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import pizza from './pizza';
 import topping from './topping';
 import person from './person';
+import storeSettings from './storeSettings';
 
 export default createSchema({
   // We name our schema
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pizza, topping, person]),
+  types: schemaTypes.concat([pizza, topping, person, storeSettings]),
 });
