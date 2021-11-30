0aeaa63d0f7e946cf810a1303dd26ec7e6e77297
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 12:02:58 -0700

    add SEO

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import PizzaList from '../components/PizzaList';
 import ToppingsFilter from '../components/ToppingsFilter';
+import SEO from '../components/SEO';
 
 export default function PizzasPage({ data, pageContext }) {
   const pizzas = data.pizzas.nodes;
   return (
     <>
+      <SEO
+        title={
+          pageContext.topping
+            ? `Pizzas With ${pageContext.topping}`
+            : `All Pizzas`
+        }
+      />
       <ToppingsFilter activeTopping={pageContext.topping} />
       <PizzaList pizzas={pizzas} />
     </>
