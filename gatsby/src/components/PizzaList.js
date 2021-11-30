2a3187d4931ec1c01c000cf9cdfe90dd6387a7d2
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 11:03:34 -0700

    add gatsby-img

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 function SinglePizza({ pizza }) {
   return (
@@ -9,6 +10,7 @@ function SinglePizza({ pizza }) {
           <span className="mark">{pizza.name}</span>
         </h2>
         <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
       </Link>
     </div>
   );
