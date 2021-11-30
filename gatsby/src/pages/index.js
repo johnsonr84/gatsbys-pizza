27bb73e8bec6f2e041aa56d543579f4867bb4318
Your Name <robertcjohnson1984@gmail.com>
2021-11-30 13:19:56 -0700

    add useLatestData

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import Nav from '../components/Nav';
-import Layout from '../components/Layout';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-      <p>Hey I'm another element</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
