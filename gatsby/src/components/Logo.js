34d92bcc587244b1864ef5a037d2f8214dbdab7a
Your Name <robertcjohnson1984@gmail.com>
2021-12-02 00:00:04 -0700

    add fonts and custom css to logo

--- gatsby/src/components/Logo.js
+++ gatsby/src/components/Logo.js
@@ -4,8 +4,10 @@ import stripes from '../assets/images/stripes.svg';
 
 const LogoStyles = styled.div`
   /* This value controls the entire size of the logo*/
+  font-family: AtlasRegular;
+  font-size: 6px;
   font-size: clamp(1px, 0.65vw, 8px);
-  width: 30em;
+  width: 55em;
   height: 30em;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
   margin: 0;
@@ -23,33 +25,39 @@ const LogoStyles = styled.div`
     align-content: center;
   }
   .est {
-    font-size: 1.5em;
+    font-family: GatsbyflfBold;
+    color: var(--black);
+    font-size: 2.8em;
     align-self: center;
   }
   h1 {
     display: grid;
+    color: var(--gold);
     grid-template-rows: 8fr 2fr;
     align-items: center;
     margin: 0;
     grid-row: 2 / span 2;
-    grid-gap: 2em;
+    ${'' /* grid-gap: 2em; */}
     transform: translateY(-0.7em);
   }
 
   .pizza {
-    font-size: 3.2em;
+    font-size: 3.8em;
+    color: #555555;
+    font-family: GatsbyflfBold;
     letter-spacing: 0.2em;
     transform: translateY(-0.15em);
   }
-  .slicks {
-    transform: scale(1.4);
+  .gatsbys {
+    transform: scale(1);
+    color: var(--gold);
     display: block;
     text-shadow: 0.18em 0.18em 0 rgba(0, 0, 0, 0.05);
-    perspective: 100px;
+    perspective: 140px;
   }
   .letter {
-    font-size: 5em;
-    color: var(--red);
+    font-size: 3.5em;
+    color: var(--black);
     --scale: 1;
     --rotate: -10deg;
     --translateX: 0;
@@ -61,37 +69,43 @@ const LogoStyles = styled.div`
     display: inline-block;
     line-height: 1;
     transition: transform 0.3s;
-    &.S {
+    &.G {
       --translateX: -0.05;
     }
-    &.l {
+    &.a {
       --rotate: 2deg;
-      --scale: 1.4;
+      --scale: 0.9;
       --translateX: 0.05em;
       --translateY: -0.05em;
     }
-    &.i {
-      --scale: 0.9;
+    &.t {
+      --scale: 1;
       --translateY: -0.1em;
       --translateX: 0.1em;
     }
-    &.c {
+    &.s {
       --rotate: 3deg;
-      --scale: 0.9;
+      --scale: 0.8;
       --translateX: 0.1em;
       --translateY: 0.23em;
     }
-    &.k {
+    &.b {
       --rotate: -12deg;
-      --scale: 1.2;
+      --scale: 1;
       --translateX: 0.06em;
+    }
+    &.y {
+      --rotate: 3deg;
+      --scale: 0.8;
+      --translateX: 0.1em;
+      --translateY: 0.23em;
     }
     &.apos {
       --translateX: 0.1em;
     }
     &.s {
       --rotate: 12deg;
-      --scale: 0.9;
+      --scale: 0.8;
       --translateY: -0.14em;
     }
   }
@@ -101,18 +115,19 @@ export default function Logo() {
   return (
     <LogoStyles className="logo">
       <div className="inner">
-        <span className="est">EST 1994</span>
+        <span className="est">EST 1925</span>
         <h1>
-          <span className="slicks">
-            <span className="letter S">S</span>
-            <span className="letter l">l</span>
-            <span className="letter i">i</span>
-            <span className="letter c">c</span>
-            <span className="letter k">k</span>
+          <span className="gatsbys">
+            <span className="letter g">G</span>
+            <span className="letter a">a</span>
+            <span className="letter t">t</span>
+            <span className="letter s">s</span>
+            <span className="letter b">b</span>
+            <span className="letter y">y</span>
             <span className="letter apos">'</span>
             <span className="letter s">s</span>
           </span>
-          <span className="pizza">pizza</span>
+          <span className="pizza">Pizza</span>
         </h1>
       </div>
     </LogoStyles>
