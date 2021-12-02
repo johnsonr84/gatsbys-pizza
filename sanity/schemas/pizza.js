09d6c33cda78f5604cd167649309f833717d8763
Your Name <robertcjohnson1984@gmail.com>
2021-12-02 00:22:02 -0700

    create PriceInput

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // Computer Name
@@ -37,7 +38,7 @@ export default {
       type: 'number',
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000),
-      // TODO: Create custom input component
+      inputComponent: PriceInput,
     },
     {
       name: 'toppings',
