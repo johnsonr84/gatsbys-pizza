ddf9ebb936464f411932822074a77821e36a1023
Your Name <robertcjohnson1984@gmail.com>
2021-12-01 23:59:15 -0700

    center footer content

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; Gatsby's Pizza {new Date().getFullYear()}</p>
+      <p className="center">&copy; Gatsby's Pizza {new Date().getFullYear()}</p>
     </footer>
   );
 }
