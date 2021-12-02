1beb6d35577d82a3b5f2ac8cbf6f2e2197b7224b
Your Name <robertcjohnson1984@gmail.com>
2021-12-02 00:14:37 -0700

    fix menu item responsiveness

--- gatsby/src/styles/MenuItemStyles.js
+++ gatsby/src/styles/MenuItemStyles.js
@@ -4,9 +4,12 @@ const MenuItemStyles = styled.div`
   display: grid;
   grid-template-columns: 100px 1fr;
   grid-template-rows: 1fr 1fr;
+  max-height: 100px;
+  min-height: 100px;
   gap: 0 1.3rem;
   align-content: center;
   align-items: center;
+  position: relative;
   .gatsby-image-wrapper {
     grid-row: span 2;
     height: 100%;
@@ -17,12 +20,13 @@ const MenuItemStyles = styled.div`
   button {
     font-size: 1.5rem;
   }
+
   button + button {
     margin-left: 1rem;
   }
   .remove {
     background: none;
-    color: var(--red);
+    color: var(--gold);
     font-size: 3rem;
     position: absolute;
     top: 0;
