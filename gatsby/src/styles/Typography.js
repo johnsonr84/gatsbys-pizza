2ac9b6afb2bbcfa980b3e2560928e1d3323537da
Your Name <robertcjohnson1984@gmail.com>
2021-12-02 00:16:26 -0700

    import custom fonts and set font styles

--- gatsby/src/styles/Typography.js
+++ gatsby/src/styles/Typography.js
@@ -1,14 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
-import font from '../assets/fonts/frenchfries.woff';
+// import font from '../assets/fonts/frenchfries.woff';
+import font from '../assets/fonts/GatsbyflfBold-owMo.ttf';
+import fonts from '../assets/fonts/AtlasRegular.ttf';
 
 const Typography = createGlobalStyle`
   @font-face {
-    font-family: FrenchFries;
+    font-family: GatsbyflfBold;
     src: url(${font});
   }
+  @font-face {
+    font-family: AtlasRegular;
+    src: url(${fonts});
+  }
   html {
-    font-family: FrenchFries, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: GatsbyflfBold, AtlasRegular, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: var(--black);
   }
   p, li {
@@ -20,12 +26,12 @@ const Typography = createGlobalStyle`
   }
   a {
     color: var(--black);
-    text-decoration-color: var(--red);
+    text-decoration-color: var(--black);
     /* Chrome renders this weird with this font, so we turn it off */
     text-decoration-skip-ink: none;
   }
   mark, .mark {
-    background: var(--yellow);
+    background: var(--gold);
     padding: 0 2px 2px 2px;
     margin: 0;
     display: inline;
