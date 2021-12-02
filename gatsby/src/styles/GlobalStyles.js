2e36e850489f92bdf465bd711e9332e678151875
Your Name <robertcjohnson1984@gmail.com>
2021-12-02 00:12:48 -0700

    update global color var

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
-import bg from '../assets/images/bg.svg';
+import bg from '../assets/images/art-deco.png';
 import stripes from '../assets/images/stripes.svg';
 
 const GlobalStyles = createGlobalStyle`
   :root {
     --red: #FF4949;
     --black: #2E2E2E;
-    --yellow: #ffc600;
+    --gold: #D4AF37;
     --white: #fff;
     --grey: #efefef;
   }
   html {
     background-image: url(${bg});
-    background-size: 450px;
+    background-size: cover;
     background-attachment: fixed;
     font-size: 10px;
   }
@@ -27,8 +27,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   button {
-    background: var(--red);
-    color: white;
+    background: var(--gold);
+    color: black;
     border: 0;
     padding: 0.6rem 1rem;
     border-radius: 2px;
@@ -40,6 +40,9 @@ const GlobalStyles = createGlobalStyle`
     &:hover {
       --cast: 4px;
     }
+  }
+  legend{
+    font-size: 3rem;
   }
 
   .gatsby-image-wrapper img[src*=base64\\,] {
@@ -53,13 +56,13 @@ const GlobalStyles = createGlobalStyle`
   }
   html {
     scrollbar-width: thin;
-    scrollbar-color: var(--red) var(--white);
+    scrollbar-color: var(--gold) var(--white);
   }
   body::-webkit-scrollbar-track {
     background: var(--white);
   }
   body::-webkit-scrollbar-thumb {
-    background-color: var(--red) ;
+    background-color: var(--gold) ;
     border-radius: 6px;
     border: 3px solid var(--white);
   }
@@ -73,6 +76,12 @@ const GlobalStyles = createGlobalStyle`
 
   img {
     max-width: 100%;
+  }
+
+  .tilt {
+    transform: rotate(-2deg);
+    position: relative;
+    display: inline-block;
   }
 
 `;
